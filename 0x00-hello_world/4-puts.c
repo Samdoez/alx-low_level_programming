27e018820001c3e3e07d8bade2cf65668ab993c2
Samdoez <samuelitoje8@gmail.com>
2022-09-08 18:02:45 -0700

    c prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - entry point
- * description: prints "\"Programming is like building a multilingual puzzle"
+ * description: prints "\"Programming is like building a multilingual puzzle"*
  * return: always 0 (success)
  */
 
