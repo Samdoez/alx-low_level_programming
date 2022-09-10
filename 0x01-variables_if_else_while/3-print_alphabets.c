92e4147dd07513baca3433baa927bdbc47f22b8e
Samdoez <samuelitoje8@gmail.com>
2022-09-09 19:58:36 -0700

    lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        char loww;
+
+for (loww = 'a'; loww <= 'z'; loww++)
+
+{       putchar(loww);
+}
+for (loww = 'A'; loww <= 'Z'; loww++)
+{
+	putchar(loww);
+	putchar('\n');
+}
+return (0);
+}
