3786d9720f6e1415cdbea79b58dd68906b419ad4
Samdoez <samuelitoje8@gmail.com>
2022-09-12 16:04:57 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{       int loww;
+for (loww = 0; loww <= 9; loww++)
+{	putchar(loww);
+}
+printf("\n");
+return (0);
+}
