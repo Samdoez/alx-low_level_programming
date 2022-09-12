f903adcf5df927f0fb511af29bd5f2cf332ec025
Samdoez <samuelitoje8@gmail.com>
2022-09-12 15:40:21 -0700

    dont print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{	char loww, e, q;
+	e = 'e';
+	y = 'q';
+
+for (loww = 'a'; loww <= 'z'; loww++)
+
+{
+	if (loww != 'e' && loww != 'q')
+{
+		putchar(loww);
+}
+printf("\n");
+return (0);
+}
