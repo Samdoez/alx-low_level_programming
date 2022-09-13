1e11ebe07e27cc3d4a1cabf4986c57f532378a25
Samdoez <samuelitoje8@gmail.com>
2022-09-12 17:37:34 -0700

    combine

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,16 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{       int n;
-for (n = 0; n < 100; n++)
-{       putchar((n / 10) + '0');
-
-	putchar((n % 10) + '0');
-	if (n != 99)
+{       int digit1, digit2;
+for (digit1 = 0; digit1 < 9; digit1++)
 {
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{	putchar((digit1 % 10) + '0');
+
+	putchar((digit2 % 10) + '0');
+	if (digit1 == 8 && digit2 == 9)
+continue;
+
 	putchar(',');
 	putchar(' ');
 }
