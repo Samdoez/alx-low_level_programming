346868b47650ddec42f6408be4b6ff820d71becd
Samdoez <samuelitoje8@gmail.com>
2022-09-14 16:39:36 -0700

    print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet_x10 - print lowercase alphabet 10 times
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{	int l, co;
+	co = 0;
+while (co < 0)
+{
+	for (l = 'a'; l <= 'z'; l++)
+{
+	_putchar(l);
+}
+}
+co++;
+_putchar('\n');
+return (0);
+}
