86d588114abd52b7789d4de5af7ca49bb707d3f8
Samdoez <samuelitoje8@gmail.com>
2022-09-23 18:27:55 -0700

    join strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,7 +18,7 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; src[i] != 0; i++)
 	{
-		dset[dlen] = src[i];
+		dest[dlen] = src[i];
 		dlen++;
 	}
 
---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - call other functions
+ *
+ * Return: int value
+ */
+int main(void)
+{
+	char *dest;
+	char *src;
+
+_strcat(dest, src);
+	return (0);
+}
