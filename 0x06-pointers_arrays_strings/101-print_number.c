f392f06cf52eec9821d410dbf084007b1e2ea171
Samdoez <samuelitoje8@gmail.com>
2022-09-23 20:42:48 -0700

    encode 2

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print any int with putchar
+ * @n: number to print
+ *
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	x = n;
+
+	if (x / 10)
+	{
+		print_number(x / 10);
+		_putchar(x % 10 + '0');
+	}
+}
