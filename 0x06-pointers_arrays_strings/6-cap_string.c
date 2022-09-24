ef89c8e94bab3c048f9addc686d7887a27bc8a03
Samdoez <samuelitoje8@gmail.com>
2022-09-23 21:07:49 -0700

    special character

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,22 @@
  */
 char *cap_string(char *s)
 {
-        int a = 0, i;
-        int spchar = 13;
-        char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	int a = 0, i;
+	int spchar = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
-        while (s[a])
-        {
-                i = 0;
-
-                while (i < spchar)
-                {
-                        if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-                        {
+	while (s[a])
+	{
+		i = 0;
+		while (i < spchar)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+			{
 				s[a] -= 32;
 			}
-                                i++;
-                }
-                a++;
-        }
-        return (s);
+			i++;
+		}
+		a++;
+	}
+	return (s);
 }
