8537cc2c18e5e97ab68e92244cd495f766c1640c
Samdoez <samuelitoje8@gmail.com>
2022-09-28 16:38:41 -0700

    Recursion

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is my recursion read me file
---
+++ 0x08-recursion/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int f;
+
+	f = factorial(n);
+	return (0);
+}
