ae3e58bad384cdb7f813c32f52f0d1142d89df8b
Samdoez <samuelitoje8@gmail.com>
2022-09-28 19:07:20 -0700

    Palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,12 +23,12 @@ int is_palindrome(char *s)
  */
 int check_palindrome(char *s)
 {
-	int l = _strlen_recursion(s) - 1;
+	int L = _strlen_recursion(s) - 1;
 
-	if (*s == s[l])
+	if (*s == s[L])
 	{
 		s++;
-		l--;
+		L--;
 	}
 	else
 	{
