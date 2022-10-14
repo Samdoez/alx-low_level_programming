8257eb8dd35e2b049d638def0a266f5cafd370e0
Samdoez <samuelitoje8@gmail.com>
2022-10-13 21:08:31 -0700

    task in 10

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - prints all
+ * @format: the style
+ *
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	f_dt form_types[] = {
+		{ "c", printf }
+	}
+}
