92bd74ce4413a515122130430feafeaa5162464a
Samdoez <samuelitoje8@gmail.com>
2022-11-04 21:27:31 -0700

    task_0x15

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,10 @@
+/**
+ * main: entry point
+ * Return: 0
+ */
+
+int main(void) 
+{
+	ssize_t read_textfile('readfile.txt', 13);
+	return (0);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main: entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	ssize_t read_textfile('readfile.txt', 13);
+        return (0);
+}
